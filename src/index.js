c97cea69abc2bf56945b32cc50fd94984b5fc5cc
Janemasha <janemasharo@gmail.com>
2021-03-14 14:10:34 +0300

    add a ready-made solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  } else {
+    array.sort(function(a, b) { 
+      return a - b;
+    })
+    return array[0]
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  } else {
+    array.sort(function(a, b) { 
+      return a - b;
+    })
+    return array[array.length - 1]
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }
+  let sum = 0;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    let avg = sum / array.length;
+    return avg
+  }
 }
